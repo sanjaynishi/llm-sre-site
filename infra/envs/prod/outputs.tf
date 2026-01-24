6f3d4dd6df8df4ae26a73017db0ca1d3d5c09201
sanjaynishi <sgoel@snrcs.com>
2026-01-24 14:43:02 -0500

    prod deployment with fixing prod.yml

--- infra/envs/prod/outputs.tf
+++ infra/envs/prod/outputs.tf
@@ -1,21 +1,21 @@
 # infra/envs/prod/outputs.tf
 
 output "agent_api_endpoint" {
-  value       = module.agent_api.http_api_endpoint
   description = "Agent API endpoint"
-}
-
-output "agent_api_http_api_id" {
-  value       = module.agent_api.http_api_id
-  description = "API Gateway HTTP API id"
+  value       = module.agent_api.agent_api_endpoint
 }
 
 output "agent_api_lambda_function_name" {
-  value       = module.agent_api.lambda_name
   description = "Lambda function name"
+  value       = module.agent_api.agent_api_lambda_function_name
 }
 
 output "ui_s3_buckets" {
-  value       = module.site.s3_buckets
   description = "Per-domain S3 buckets used for UI hosting"
+  value       = module.site.ui_s3_buckets
+}
+
+output "cloudfront_domains" {
+  description = "Per-domain CloudFront distribution domain names"
+  value       = module.site.cloudfront_domains
 }
