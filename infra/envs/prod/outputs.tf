3576b9e9e878905aa601ae59e9a386637e953e65
sanjaynishi <sgoel@snrcs.com>
2026-01-24 14:51:27 -0500

    prod deployment with outputs.tf file

--- infra/envs/prod/outputs.tf
+++ infra/envs/prod/outputs.tf
@@ -1,21 +1,31 @@
 # infra/envs/prod/outputs.tf
 
 output "agent_api_endpoint" {
+  value       = module.agent_api.http_api_endpoint
   description = "Agent API endpoint"
-  value       = module.agent_api.agent_api_endpoint
 }
 
 output "agent_api_lambda_function_name" {
+  value       = module.agent_api.lambda_function_name
   description = "Lambda function name"
-  value       = module.agent_api.agent_api_lambda_function_name
-}
-
-output "ui_s3_buckets" {
-  description = "Per-domain S3 buckets used for UI hosting"
-  value       = module.site.ui_s3_buckets
 }
 
 output "cloudfront_domains" {
-  description = "Per-domain CloudFront distribution domain names"
   value       = module.site.cloudfront_domains
+  description = "Per-domain CloudFront distribution domains"
+}
+
+output "ui_s3_buckets" {
+  value       = module.site.s3_buckets
+  description = "Per-domain S3 buckets used for UI hosting"
+}
+
+output "agent_config_bucket" {
+  value       = var.agent_config_bucket
+  description = "S3 bucket used for agent config + runbooks + vectors"
+}
+
+output "agent_config_prefix" {
+  value       = var.agent_config_prefix
+  description = "Prefix for agent config JSON"
 }
